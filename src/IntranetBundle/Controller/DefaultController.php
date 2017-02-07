299cfa3f27503f7f2645bbb1ea98d9e1b7aac1d5
Mobuis <mobuis-ro77@live.fr>
2017-02-07 01:59:02 +0100

    [ADD&FIX] new homepage + delete broken 404 redirect

--- src/IntranetBundle/Controller/DefaultController.php
+++ src/IntranetBundle/Controller/DefaultController.php
@@ -77,23 +77,4 @@ class DefaultController extends Controller
             ->findById($id);
         return $this->render('IntranetBundle:Default:userProfil.html.twig', array("user" => $user));
     }
-
-    /**
-     * @Route("/{url}", name="remove_trailing_slash",
-     *     requirements={"url" = ".*\/$"}, methods={"GET"})
-     */
-    public function removeTrailing(Request $request)
-    {
-        return $this->render('IntranetBundle:Default:index.html.twig');
-    }
-
-    public function removeTrailingSlashAction(Request $request)
-    {
-        $pathInfo = $request->getPathInfo();
-        $requestUri = $request->getRequestUri();
-
-        $url = str_replace($pathInfo, rtrim($pathInfo, ' /'), $requestUri);
-
-        return $this->redirect($url, 404);
-    }
 }
